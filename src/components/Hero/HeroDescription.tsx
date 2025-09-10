6514351c945463181e69d919744041722090eddd
Hadi Ahmad <ahmad287@umn.edu>
2025-09-10 12:41:07 -0500

    added metadata and favicon.ico

--- src/components/Hero/HeroDescription.tsx
+++ src/components/Hero/HeroDescription.tsx
@@ -1,7 +1,7 @@
 export default function HeroDescription() {
   return (
     <p className="mt-4 text-base sm:text-md text-foreground/70 font-mono max-w-xl">
-      <span className="block mb-2">I'm interested in building scalable applications, and learning new technologies.</span>
+      <span className="block mb-2">I enjoy creating solutions to real-world problems and learning new things while I do it.</span>
       <span className="block">I'm currently pursuing a B.S in Computer Science and Computer Engineering at the University of Minnesota - Twin Cities.</span>
     </p>
   );
